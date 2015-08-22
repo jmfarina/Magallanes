2765174f5e813ddaae4bf5eb18fc0f3e62231611
Juan Farina <jfarina@agminen.com>
2015-08-22 18:43:21 -0300

    - renamed package to avoid counter-intuitive name for task definition in config file, since Magallanes transforms tasks' names to camelcase.

--- Mage/Task/BuiltIn/NodeJs/BowerTask.php
+++ Mage/Task/BuiltIn/NodeJs/BowerTask.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Mage\Task\BuiltIn\NodeJS;
+namespace Mage\Task\BuiltIn\NodeJs;
 
 use Mage\Task\AbstractTask;
 
